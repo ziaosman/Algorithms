e2ca5a39dcf6039aaeec4f225b6f239ac7ad99cc
zia <fowziya@zias-MacBook-Air.local>
2018-08-09 20:01:34 -0500

    Cleaned up formatting and comments

--- Percolation.java
+++ Percolation.java
@@ -1,7 +1,6 @@
 //Zia
+//Defines percolation class 
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation
@@ -13,65 +12,74 @@ public class Percolation
    //create n-by-n grid, with all sites blocked
    public Percolation(int n)
    {
-   	gridSize = n*n;
-   	grid = new WeightedQuickUnionUF(gridSize+2);
-   	gridStatus= new Boolean(gridSize);
+   		gridSize = n*n;
+   		grid = new WeightedQuickUnionUF(gridSize+2);
+   		gridStatus= new Boolean(gridSize);
 
-   	//indexes of top and bottom row in array
-   	topRow = n*n+1;
-   	bottomRow = n*n+2;
+   		//indexes of top and bottom row in array
+   		topRow = n*n+1;
+   		bottomRow = n*n+2;
    }
-   //open site (row, col) if it is not open already
+
+   //Opens a site on the grid
+   //Param: site on the grid
+   //Throw: IllegalArgumentException if site is not on the grid
    public void open(int row, int col)
    {
-   	if (!isLegal(row,col))
-   	{
-   	throw new IllegalArgumentException("Sorry, Those values are not valid");
-   	}
-   	gradStatus[index(row, col)] = true;
-   }
-   //is site (row, col) open?
-   public boolean isOpen(int row, int col) 
-   {
-   	if (!isLegal(row,col))
+   		if (!isLegal(row,col))
    		{
    			throw new IllegalArgumentException("Sorry, Those values are not valid");
    		}
-   	return gradStatus[index(row, col)];
+   		gradStatus[index(row, col)] = true;
    }
-    //is site (row, col) full?
+
+   //Returns True if the site is open
+   //Throw: IllegalArgumentException if site is not on the grid
+   public boolean isOpen(int row, int col) 
+   {
+   		if (!isLegal(row,col))
+   		{
+   			throw new IllegalArgumentException("Sorry, Those values are not valid");
+   		}
+   		return gradStatus[index(row, col)];
+   }
+
+   //Returns True a site is connected to the top row
+   //Throw: IllegalArgumentException if site is not on the grid
    public boolean isFull(int row, int col) 
    {
-   	if (!isLegal(row,col))
-   	{
-   		throw new IllegalArgumentException("Sorry, Those values are not valid");
-   	}
-   	return gridSize.connected[index(row,col),topRow];
+   		if (!isLegal(row,col))
+   		{
+   			throw new IllegalArgumentException("Sorry, Those values are not valid");
+   		}
+   		return gridSize.connected[index(row,col),topRow];
    }
-   // number of open sites
+
+   //Returns the number of open sites in the grid
    public int numberOfOpenSites()
    {
-   	counter = 0;
-   	for (int i : gridStatus)
-   	{
-   		if (gridStatus[i] == true)
+   		counter = 0;
+		for (int i : gridStatus)
    		{
+   			if (gridStatus[i] == true)
    			counter++
    		}
-   	}
-   	return counter;
-   }
-   // does the system percolate?
+		return counter;
+	}
+
+   //Returns True if the grid percolates 
    public boolean percolates()
    {
-   	return gridSize.connected(topRow,bottomRow);
+   		return gridSize.connected(topRow,bottomRow);
    }
-   //calculates index of grid on the weighted grid 
+
+   //Calculates index of site in the WeightedQuickUnionUF array called grid
    private int index(int row, int col)
 	{
 		return (n*(i-row)+col)-1;
 	}
-	//calculates whether a grid coordinate is legal 
+
+	//Returns true if site is located on the grid
 	private boolean isLegal(int row, int col)
 	{
 		if((row < 1 || row > n) || (col < 1 || col > n))
@@ -80,8 +88,8 @@ public class Percolation
 		}
 		return true;
 	}
+}
    public static void main(String[] args)
 	{
-		//test client
+		//Test client
 	}
-}
