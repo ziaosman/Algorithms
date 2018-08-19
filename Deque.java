c6326253973c5c1eb4c866862b80e5d5ca10ad4b
zia <fowziya@zias-MacBook-Air.local>
2018-08-19 13:50:55 -0500

    Began implementation of a deque

---
+++ Deque.java
@@ -0,0 +1,61 @@
+//Zia Osman
+
+public class Deque<Item> implements Iterable<Item>
+{
+	// construct an empty deque
+   public Deque()
+   {
+
+   } 
+   // is the deque empty?
+   public boolean isEmpty()
+   {
+
+   }
+    // return the number of items on the deque
+   public int size()
+   {
+
+   }
+
+   // add the item to the front                      
+   public void addFirst(Item item)  
+   {
+
+   }
+   // add the item to the end        
+   public void addLast(Item item)  
+   // remove and return the item from the front      
+   public Item removeFirst()
+   {
+   	if (isEmpty())
+   	{
+   		throw new IllegalArgumentException("The deque is already empty");
+   	}
+   	else if()
+   	{
+   		//TODO
+   	}
+
+   }
+   // remove and return the item from the end              
+   public Item removeLast()
+   {
+   	if (isEmpty())
+   	{
+   		throw new IllegalArgumentException("The deque is already empty");
+   	}
+   	else if ()
+   	{
+   		//TODO
+   	}
+   }
+
+   // return an iterator over items in order from front to end                 
+   public Iterator<Item> iterator() 
+   {
+
+   }
+   //Test Client
+   public static void main(String[] args)   
+}
